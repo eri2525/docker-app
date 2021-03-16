e27b3a050c04eaf422ca290be178a01be956381f
eri2525 <71331326+eri2525@users.noreply.github.com>
2021-03-16 12:41:52 +0900

    ユーザーモデルの認証機能設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+  prsence: true,
+  uniqueness: true,
+  length: { maximum: 16 },
+  format: {
+    with: /\[a-z0-9]+\z/,
+    message: 'は小文字英数字で入力してください'
+  }
+  validates :password,
+  length: { minimum: 8 }
 end
